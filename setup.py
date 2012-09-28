35d9b12c9e24bca16135a6e3b2769f883d29e7e2
Denis Veselov <veselov@trilan.ru>
2012-09-28 16:02:24 +0400

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='django-simple-open-graph',
-    version='0.2',
+    version='0.3',
     license='ISC',
     description='Django package for simplicity embed open graph (og:) '
                 'layout in templates for different objects',
